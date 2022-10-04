c6c89698f34ca9c729a0bd24519097f69c0ffa86
tetsugps <imagawa77@gmail.com>
2022-10-04 10:13:02 +0900

    atom updated

--- feedgene.py
+++ feedgene.py
@@ -1,13 +1,30 @@
 from feedgen.feed import FeedGenerator
+import datetime
+import pytz
+
+unaware = datetime.datetime(2011, 8, 15, 8, 15, 12, 0)
+aware = datetime.datetime(2011, 8, 15, 8, 15, 12, 0, pytz.UTC)
+
+logo = "https://prtimes.jp/data/corp/40420/tmp-9c9afdfe87f255d43762faf051399838-4146237a3477a4f2e775a05b4fa06df7.jpg"
+url = "https://t-i.jp"
 
 fg = FeedGenerator()
-fg.id('http://lernfunk.de/media/654321')
+fg.id("0")
 fg.title('テスト')
-fg.author({'name': 'John Doe', 'email': 'john@example.de'})
-fg.link(href='http://example.com', rel='alternate')
-fg.logo('http://ex.com/logo.jpg')
-fg.subtitle('This is a cool feed!')
-#fg.link(href='http://larskiesow.de/test.atom', rel='self')
+fg.author({'name': 'TI Inc.', 'email': 'info@t-i.jp'})
+fg.link(href=url, rel='alternate')
+fg.logo(logo)
+fg.subtitle('フィードのタイトルです')
+fg.link(href=url, rel='self')
 fg.language('ja')  # iso639
+fg.image(url)
+
+fe = fg.add_entry()
+fe.id("1")
+fe.title('エントリー１')
+fe.link(href=url)
+fe.pubDate(aware)
+fe.content("content")
+fe.description("description")
 
 fg.atom_file("atom.xml")
